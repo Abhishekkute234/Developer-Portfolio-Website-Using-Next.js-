a28bf0a785a2a5f10617db16c7d5757c23f39c89
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-11-03 10:09:25 +0530

    set up the environment for the next.js and the tailwind Css

---
+++ developer_portfolio/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 "> 
+    {/* the main is similar to the dev component  */}
+
+    <div>
+      <h1>Hello Portfolio </h1>
+    </div>
+    </main>
+  );
+}
